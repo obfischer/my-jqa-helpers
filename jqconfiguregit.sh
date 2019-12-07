5c17e1139d4e9d0d07e52734fabc9db38b10d57f
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2019-12-07 21:42:35 +0100

    Skript zur einheitlichen Konfiguration der ausgecheckten Git-Repositorys hinzugefügt

---
+++ jqconfiguregit.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# See https://stackoverflow.com/questions/59895/
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source ${script_dir}/colors.sh
+source ${script_dir}/projects.sh
+
+for subproject in ${subprojects[*]}
+do
+    echo
+    echo "${GREEN}###${RESET}"
+    echo "${GREEN}### Configuring Git $subproject via ${RESET}"
+    echo "${GREEN}###${RESET}"
+    echo
+
+    pushd ~/code/jqa/${subproject}
+    configure-git-private || exit 1
+    popd
+done
+
+
